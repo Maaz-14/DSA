16ba72d65755c5d4bc48236d50ba820216794735
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2025-01-19 03:01:31 +0500

    Update Smallest Element.cpp

--- Arrays/Smallest Element.cpp
+++ Arrays/Smallest Element.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int marks[]={-105,100,105,110,-108};
-    int min = INT_MAX; // plus infinity !!!
-    for(int i=0;i<(sizeof(marks)/sizeof(marks[0]));i++){
-        if(min>marks[i])
-            min = marks[i];
+void smallestElement(int *arr, int size){
+    int smallest = INT_MAX;
+    for(int i=0;i<size;i++){
+        smallest = min(arr[i],smallest);// This is a built-in function in C++ to find the minimum of two numbers.
+        //max(arr[i],largest) // for maximum element.
     }
-    cout<<min<<endl;
+    cout<<smallest<<endl;
+}
+int main(){
+    int marks[]={99,100,105,110,102};
+    
+    smallestElement(marks,5);
+
+    int smallest = INT_MAX; // INT_max means plus infinity !!!
+    for(int i=0;i<(sizeof(marks)/sizeof(marks[0]));i++){
+        if(smallest>marks[i]) // OR smallest = min(marks[i],smallest);
+            smallest = marks[i];
+    }
+    cout<<smallest<<endl;
 
     return 0;
 }
