4451be8a29d9f2ea066ea2a9da6fda2c0ebd61a2
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2025-01-19 02:33:33 +0500

    Update Smallest Element.cpp

--- Arrays/Smallest Element.cpp
+++ Arrays/Smallest Element.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int marks[]={-105,100,105,110,-108};
+    int min = INT_MAX; // plus infinity !!!
+    for(int i=0;i<(sizeof(marks)/sizeof(marks[0]));i++){
+        if(min>marks[i])
+            min = marks[i];
+    }
+    cout<<min<<endl;
 
+    return 0;
+}
