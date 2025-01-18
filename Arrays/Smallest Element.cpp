a9af1c40a7994ea8c87d8c600aa8f9bea673b442
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2025-01-19 03:17:16 +0500

    Update Smallest Element.cpp

--- Arrays/Smallest Element.cpp
+++ Arrays/Smallest Element.cpp
@@ -22,3 +22,30 @@ int main(){
 
     return 0;
 }
+// Program : With the index on which smallest and Largest number are found
+#include <iostream>
+using namespace std;
+void solve(){
+    int largest = INT_MIN;
+    int smallest = INT_MAX;
+    int  array [] = {0,9,-9,8,2,22,56,-5,90};
+    int size = sizeof(array)/sizeof(array[0]);
+    int SmallestNumberIndex;
+    int LargestNumberIndex;
+    for(int i=0;i<size;i++){
+        smallest = min(smallest,array[i]);
+        if(smallest == array[i]) SmallestNumberIndex = i;
+        
+        largest = max(largest,array[i]);
+        if(largest == array[i]) LargestNumberIndex = i;
+    }
+    cout<<"The smallest number is "<<smallest<<" which is on index "<<SmallestNumberIndex<<endl;
+    cout<<"The largest number is "<<largest<<" which is on index "<<LargestNumberIndex<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
