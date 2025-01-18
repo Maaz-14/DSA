0d3dfdb55014e44fd10213b37cb319b59df68b83
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2025-01-19 03:53:43 +0500

    Update Linear_Search.cpp

--- Arrays/Linear_Search.cpp
+++ Arrays/Linear_Search.cpp
@@ -1,1 +1,20 @@
-
+#include<bits/stdc++.h>
+using namespace std;    
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define for(i,n) for(int i=0;i<n;i++)
+int linearSearch(int arr[], int size){
+    int targetElement;
+    cin>>targetElement;
+    for(i,size){
+        if(arr[i]==targetElement)
+            return i; // If the element is found return the index of the element
+    }
+    return -1; // If the element is not found
+}
+int main(){
+    fast;
+    int array[]= {9,8,2,7,1,2,3};
+    cout<<"Value found at index "<<linearSearch(array,7)<<endl;
+    return 0;
+}
+// Time complexity of linear search is "O(n)" where n is the number of elements in the array.
