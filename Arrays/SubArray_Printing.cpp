99de02349217a47ee695230b60155cb5f546827e
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2025-01-28 19:33:53 +0500

    Update SubArray_Printing.cpp

--- Arrays/SubArray_Printing.cpp
+++ Arrays/SubArray_Printing.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n =5;
+    int array[n] = {1,2,3,4,5};
+    for(int start = 0;start<n; start++){//1
+        for(int end = start; end<n; end++){//1 2 3 4 
+            for(int i=start; i<=end;i++){
+                cout<<array[i];// 2 23 234 2345
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}          
