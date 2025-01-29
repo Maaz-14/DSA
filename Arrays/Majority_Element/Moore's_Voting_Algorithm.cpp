a0433d4a176e2850a17ccc46bd7fe1712076749a
Maaz-14 <110842425+Maaz-14@users.noreply.github.com>
2025-01-30 01:15:12 +0500

    Create Moore's_Voting_Algorithm.cpp

---
+++ Arrays/Majority_Element/Moore's_Voting_Algorithm.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n= nums.size();
+        int freq =0 , ans =0;
+        for(int i=0;i<n;i++){
+            if(freq==0)
+                ans = nums[i];
+            if(ans == nums[i])
+                freq++;
+            else 
+                freq--;
+        }
+        return ans;
+    }
+};// Assuming Majority ELements exists!!
+// See notebook for Edge Cases of Moore's Algorithm
